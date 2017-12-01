158585ae114c84789da6825984352c1239f96e3d
Aoife Fogarty <afogarty1000@yahoo.com>
2017-12-01 23:43:35 +0100

    test first model for spelt->phones

--- cmudict-model.py
+++ cmudict-model.py
@@ -4,6 +4,8 @@ import itertools
 import string
 import re
 from sklearn import preprocessing
+import sklearn.linear_model as lm
+from sklearn.model_selection import train_test_split
 
 
 def main():
@@ -17,12 +19,25 @@ def main():
   # get all letters that could be in speltWords
   letters = list(string.printable)
 
-  encoded_X = encode(speltWords, letters)
+  encoded_X, speltEncoder = encode(speltWords, letters)
+  encoded_Y, phonesEncoder = encode(phoneticWords, phonesList)
 
-  encoded_Y = encode(phoneticWords, phonesList)
 
-  print(encoded_Y[45:50])
+  ols = lm.LinearRegression()
+  model = ols.fit(encoded_X, encoded_Y)
+  predicted_Y = model.predict(encoded_X)
+
+  print(model.score(encoded_X, encoded_Y))
+
+  decoded_Y = decode(encoded_Y, phonesEncoder)
+  print(encoded_Y[:5])
+  print(predicted_Y[:5])
+  #same shape
+  #but labels not integers any more!
+  decoded_Y = decode(predicted_Y, phonesEncoder)
+  print(decoded_Y[45:50])
   print(phoneticWords[45:50])
+
 
 
 def loadData():
@@ -75,8 +90,45 @@ def encode(listListSymbols, symbolsSet):
 
   encoded_X = np.apply_along_axis(le.transform, 1, X)
 
-  return encoded_X
+  return encoded_X, le
 
 
-if __name__ == '__main__':
-  main() 
+def decode(encoded_X, le):
+
+  decoded_X = np.apply_along_axis(le.inverse_transform, 1, encoded_X)
+
+  return decoded_X
+
+test_fraction = 0.9
+# get list of phones used, and labelled data (spelling to phonetic spelling)  
+phonesList, speltWords, phoneticWords = loadData()
+
+# list of strings to list of lists
+speltWords = list(map(list, speltWords))
+
+# get all letters that could be in speltWords
+letters = list(string.printable)
+
+encoded_X, speltEncoder = encode(speltWords, letters)
+encoded_Y, phonesEncoder = encode(phoneticWords, phonesList)
+
+X_train, X_test, Y_train, Y_test = train_test_split(encoded_X, encoded_Y, test_size=test_fraction, random_state=0)
+
+ols = lm.LinearRegression()
+model = ols.fit(X_train, Y_train)
+Y_predicted = model.predict(X_train)
+Y_predicted = Y_predicted.astype(int)
+nPhones = len(phonesEncoder.classes_) - 1
+Y_predicted[Y_predicted > nPhones] = nPhones
+
+print(model.score(X_train, Y_train))
+
+#Y_train_decoded = decode(Y_train, phonesEncoder)
+#print(Y_train[:5])
+#print(Y_train_decoded[:5])
+Y_predicted_decoded = decode(Y_predicted, phonesEncoder)
+print(Y_predicted_decoded[45:50])
+print(phoneticWords[45:50])
+
+#if __name__ == '__main__':
+#  main() 
